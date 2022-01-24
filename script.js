fa00baaa8433841c06fd661990ed90be90415c40
Jason Miller <>
2022-01-24 09:02:06 +0000

    refactor! much cleaner grid, add AND remove

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let hueFamilies = 3;
 let hue;
 let startHue = 200;
 let endHue = 210;
-let saturation100;
+let saturation100 = 100;
 let saturation;
 let contrast;
 let lightness = 0;
@@ -29,133 +29,215 @@ let contrasts = [];
 let hues = [];
 
 
-function addHueFamily() {
-  hueFamilies++;
+//updateUI should contain functions like update contrast settings, update hue settings, update swatches
+function initializeUI() {
+  updateUI();
+  document.getElementById("step0").value = 3;
+  document.getElementById("step1").value = 4.5;
+  document.getElementById("step2").value = 7;
+  document.getElementById("fam0").value = 350;
+  document.getElementById("fam1").value = 160;
+  document.getElementById("fam2").value = 220;
+  updateUI();
+  //updateSwatches();
+}
 
-  let newHueCell = document.createElement("div");
-  let newHueCellLabel = document.createElement("label");
-  let newHueCellLabelText = document.createTextNode("Hue:");
-  let newHueCellInput = document.createElement("input");
-  //create hueCell div, label, and input
-
-  newHueCellInput.type = "number";
-  newHueCellInput.name = "hue";
-  newHueCellInput.value = "0";
-  newHueCellInput.addEventListener("change", updateColors);
-  newHueCellInput.min = "0";
-  newHueCellInput.max = "360";
-  //add attributes to hueCell input
-
-  newHueCell.appendChild(newHueCellLabel); //put label inside div
-  newHueCellLabel.appendChild(newHueCellLabelText); //put label text inside label
-  newHueCell.appendChild(newHueCellInput); //put input inside div
-
-  let addHue = document.getElementById("addHue");
-  document.getElementById("grid").insertBefore(newHueCell, addHue);
-  //put hueCell before the existing Add Hue Family button
-  //console.log(gridCols, contrastSteps);
-
-  for (let col = 0; col < contrasts.length; col++) {
-    let newSwatch = document.createElement("div");
-    newSwatch.id = "swatchR" + (hueFamilies-1) + "C" + col;
-    newSwatch.className = "swatch";
-    newSwatch.style.backgroundColor = "white";
-    document.getElementById("grid").insertBefore(newSwatch, addHue);
-    //similar to above, create swatch divs and add to new row, for each col
-    //console.log(row, contrastSteps, newSwatch.id, newSwatch.className, newSwatch.style.backgroundColor, rowEndSwatch.id);
-  }
-
-  let newEndCell = document.createElement("div");
-  newEndCell.id = "swatchR" + (hueFamilies-1) + "End";
-  newEndCell.className = "swatch";
-  document.getElementById("grid").insertBefore(newEndCell, addHue);
-
-  updateColors();   
+function updateUI() {
+  updateContrastSettings();
+  updateHueSettings();
+  updateSwatches();
 }
 
 function addContrastStep() {
   contrastSteps++;
-  gridCols = contrastSteps + 2;
-  document.getElementById("grid").style.gridTemplateColumns = "repeat(" + gridCols + ", 1fr)";
-  //console.log(gridCols, contrastSteps);
-  //restyle grid to have 1 additional column
-
-  let newContrastCell = document.createElement("div");
-  let newContrastCellLabel = document.createElement("label");
-  let newContrastCellLabelText = document.createTextNode("Target contrast:");
-  let newContrastCellInput = document.createElement("input");
-  //create contrastCell div, label, and input
-
-  newContrastCellInput.type = "number";
-  newContrastCellInput.name = "contrast";
-  newContrastCellInput.value = "2";
-  newContrastCellInput.addEventListener("change", updateColors);
-  newContrastCellInput.min = "1";
-  newContrastCellInput.max = "21";
-  newContrastCellInput.step = "0.1";
-  //add attributes to contrastCell input
-
-  newContrastCell.appendChild(newContrastCellLabel); //put label inside div
-  newContrastCellLabel.appendChild(newContrastCellLabelText); //put label text inside label
-  newContrastCell.appendChild(newContrastCellInput); //put input inside div
-
-  let addContrast = document.getElementById("addContrast");
-  document.getElementById("grid").insertBefore(newContrastCell, addContrast);
-  //put contrastCell before the existing Add Contrast Step button
-  //console.log(gridCols, contrastSteps);
-
-  for (let row = 0; row < hues.length; row++) {
-    let newSwatch = document.createElement("div");
-    newSwatch.id = "swatchR" + row + "C" + (contrastSteps-1);
-    newSwatch.className = "swatch";
-    newSwatch.style.backgroundColor = "white";
-    let rowEndSwatch = document.getElementById("swatchR" + row + "End");
-    document.getElementById("grid").insertBefore(newSwatch, rowEndSwatch);
-    //similar to above, create swatch div and add to end of row, for each row
-    //console.log(row, contrastSteps, newSwatch.id, newSwatch.className, newSwatch.style.backgroundColor, rowEndSwatch.id);
-  }
-
-  updateColors(); //this generates a weird error i can't fix, but seems to work fine regardless for some reason
+  contrasts.push(2);
+  updateContrastSettings();
+  updateSwatches();
 }
 
-function updateColors() {
-  saturation100 = parseFloat(document.getElementById('saturation').value);
+function delContrastStep(event) {
+  if (contrastSteps > 1) {
+    let contrastSetting = this.parentNode.id;
+    document.getElementById(contrastSetting).remove();
+    contrastSteps--;
+    updateContrastSettings();
+    updateSwatches();
+  }
+}
 
+function addHueFamily() {
+  hueFamilies++;
+  hues.push(0);
+  updateHueSettings();
+  updateSwatches();
+}
+
+function delHueStep(event) {
+  if (hueFamilies > 1) {
+    let hueSetting = this.parentNode.id;
+    console.log(hueSetting);
+    document.getElementById(hueSetting).remove();
+    hueFamilies--;
+    updateHueSettings();
+    updateSwatches();
+  }
+}
+
+function updateContrastSettings() {
   let contrastValues = document.getElementsByName("contrast");
   for (let step = 0; step < contrastValues.length; step++) {
     contrasts[step] = contrastValues[step].value;
+  } //update contrasts array with current contrast values
+  //console.log(contrasts);
+
+  let contrastSettings = document.getElementById("contrastSettings");
+  while (contrastSettings.firstChild) {
+    contrastSettings.removeChild(contrastSettings.lastChild);
+  } //remove all existing contents of grid so we can start fresh
+
+  contrastSettings.style.gridTemplateColumns = "repeat(" + contrastSteps + ", 1fr)";
+  grid.style.gridTemplateColumns = "repeat(" + contrastSteps + ", 1fr)";
+  for (let step = 0; step < contrastSteps; step++) {
+    createContrastSetting(step); //create setting UI for each step
+
+    let id = "step" + step;
+    document.getElementById(id).value = contrasts[step];
+    console.log(id, contrastValues[step].value);
+    //add back previous contrast values
   }
+}
+
+function updateHueSettings() {
   let hueValues = document.getElementsByName("hue");
-  for (let step = 0; step < hueValues.length; step++) {
-    hues[step] = hueValues[step].value;
+  for (let i = 0; i < hueValues.length; i++) {
+    hues[i] = hueValues[i].value;
+  } //update hues array with current hue values
+  //console.log(hues);
+
+  let hueSettings = document.getElementById("hueSettings");
+  while (hueSettings.firstChild) {
+    hueSettings.removeChild(hueSettings.lastChild);
+  } //remove all existing contents of grid so we can start fresh
+
+  hueSettings.style.gridTemplateRows = "repeat(" + hueFamilies + ", 1fr)";
+  //grid.style.gridTemplateRows = "repeat(" + hueFamilies + ", 1fr)";
+  for (let fam = 0; fam < hueFamilies; fam++) {
+    createHueSetting(fam); //create setting UI for each step
+
+    let id = "fam" + fam;
+    document.getElementById(id).value = hues[fam];
+    //console.log(id, hueValues[fam].value);
+    //add back previous contrast values
   }
-  for (let row = 0; row < hues.length; row++) {
-    for (let cell = 0; cell < contrasts.length; cell++) {
-      let color = getContrast(hues[row], saturation100, contrasts[cell]);
-      //let swatch = "swatch" + ((row * 3) + cell + 1);
-      let swatch = "swatchR" + row + "C" + cell;
-      document.getElementById(swatch).style.backgroundColor = color;
+}
 
-      let actualContrast = Math.round(chroma.contrast(color, "white") * 100)/100; 
-      let swatchContrast = "contrastR" + row + "C" + cell; 
-      document.getElementById(swatchContrast).innerHTML = "Contrast: " + actualContrast;
-      //get actual contrast of color and display it inside the swatch
-
-      if (actualContrast < 3) {
-        document.getElementById("nameR" + row + "C" + cell).style.color = "black";
-        document.getElementById("satR" + row + "C" + cell).style.color = "black";
-        document.getElementById(swatchContrast).style.color = "black";
-      } else {
-        document.getElementById("nameR" + row + "C" + cell).style.color = "white";
-        document.getElementById("satR" + row + "C" + cell).style.color = "white";
-        document.getElementById(swatchContrast).style.color = "white";
-      }
-
-      console.log(contrasts.length, hues.length, row, cell, swatch, actualContrast, swatchContrast);
+function updateSwatches() {
+  let grid = document.getElementById("grid");
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  } //remove all existing contents of grid so we can start fresh
+  
+  for (let fam = 0; fam < hueFamilies; fam++) {
+    for (let step = 0; step < contrastSteps; step++) {
+      createSwatch(fam, step);
     }
   }
 }
 
+function createContrastSetting(step) {
+  let contrastSetting = document.createElement("div");
+  let contrastSettingLabel = document.createElement("label");
+  let contrastSettingLabelText = document.createTextNode("Target contrast: ");
+  let contrastSettingInput = document.createElement("input");
+  let contrastSettingDelete = document.createElement("button");
+  let contrastSettingDeleteText = document.createTextNode("Delete");
+  //create contrastSetting contents
+
+  contrastSetting.id = "contrastSetting" + step;
+  contrastSettingInput.id = "step" + step;
+  contrastSetting.className = "contrastSetting";
+  contrastSettingInput.type = "number";
+  contrastSettingInput.name = "contrast";
+  //contrastSettingInput.value = "2";
+  contrastSettingInput.addEventListener("change", updateSwatches);
+  contrastSettingInput.addEventListener("click", contrastSettingInput.select);
+  contrastSettingInput.min = "1";
+  contrastSettingInput.max = "21";
+  contrastSettingInput.step = "0.1";
+  contrastSettingDelete.addEventListener("click", delContrastStep);
+  //add attributes to contents
+
+  document.getElementById("contrastSettings").appendChild(contrastSetting);
+  contrastSetting.appendChild(contrastSettingLabel);
+  contrastSettingLabel.appendChild(contrastSettingLabelText);
+  contrastSetting.appendChild(contrastSettingInput);
+  contrastSetting.appendChild(contrastSettingDelete);
+  contrastSettingDelete.appendChild(contrastSettingDeleteText);
+  //put contents in DOM
+}
+
+function createHueSetting(fam) {
+  let hueSetting = document.createElement("div");
+  let hueSettingLabel = document.createElement("label");
+  let hueSettingLabelText = document.createTextNode("Hue: ");
+  let hueSettingInput = document.createElement("input");
+  let hueSettingDelete = document.createElement("button");
+  let hueSettingDeleteText = document.createTextNode("Delete");
+  //create hueSetting contents
+
+  hueSetting.id = "hueSetting" + fam;
+  hueSettingInput.id = "fam" + fam;
+  hueSetting.className = "hueSetting";
+  hueSettingInput.type = "number";
+  hueSettingInput.name = "hue";
+  //hueSettingInput.value = "0";
+  hueSettingInput.addEventListener("change", updateSwatches);
+  hueSettingInput.addEventListener("click", hueSettingInput.select);
+  hueSettingInput.min = "-360";
+  hueSettingInput.max = "360";
+  hueSettingDelete.addEventListener("click", delHueStep);
+  //add attributes to contents
+
+  document.getElementById("hueSettings").appendChild(hueSetting);
+  hueSetting.appendChild(hueSettingLabel);
+  hueSettingLabel.appendChild(hueSettingLabelText);
+  hueSetting.appendChild(hueSettingInput);
+  hueSetting.appendChild(hueSettingDelete);
+  hueSettingDelete.appendChild(hueSettingDeleteText);
+  //put contents in DOM
+}
+
+function createSwatch(fam, step) {
+  let hueID = "fam" + fam; //get id of corresponding hue input
+  let hueValue = document.getElementById(hueID).value; //get value of hue input
+  //console.log(hueID, hueValue);
+  let contrastID = "step" + step; //get id of corresponding contrast input
+  let contrastValue = document.getElementById(contrastID).value; //get value of contrast input
+  let color = getContrast(hueValue, saturation100, contrastValue); //get color
+  let actualContrast = Math.round(chroma.contrast(color, "white") * 100)/100; //get actual contrast of color 
+  //console.log(hueID, hueValue, contrastID, contrastValue, saturation100);
+
+  let swatch = document.createElement("div");
+  let swatchContrast = document.createElement("p");
+  let swatchContrastText = document.createTextNode("Contrast: " + actualContrast);
+  //create swatch contents
+
+  swatch.id = "swatchF" + fam + "S" + step;
+  swatch.className = "swatch";
+  swatch.style.backgroundColor = color;
+  swatchContrast.className = "swatchText";
+  if (actualContrast < 4.5) {
+    swatchContrast.style.color = "black";
+  } else { swatchContrast.style.color = "white"; }
+  //add attributes to contents
+
+  document.getElementById("grid").appendChild(swatch);
+  swatch.appendChild(swatchContrast);
+  swatchContrast.appendChild(swatchContrastText);
+  //put contents in DOM
+}
+  
+//////////////////////////////////////////////
 
 
 function getContrast(hue, sat100, contrast) {
